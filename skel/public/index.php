f6623b24ea2c7584498e03a8d268a8e58914b595
Taiji Inoue <inudog@gmail.com>
2013-01-23 06:37:48 +0900

    path changed

--- skel/public/index.php
+++ skel/public/index.php
@@ -4,12 +4,14 @@
  *
  */
 
-define("MOF_ROOT_DIR", dirname(__DIR__));
-define("MOF_VIEW_DIR", MOF_ROOT_DIR . '/view');
-define("MOF_CONFIG_DIR", MOF_ROOT_DIR . '/config');
-define("MOF_CONTROLLER_DIR", MOF_ROOT_DIR . '/controller');
+define('MOF_APP_DIR', dirname(__DIR__));
+define('MOF_VIEW_DIR', MOF_APP_DIR . '/view');
+define('MOF_CONFIG_DIR', MOF_APP_DIR . '/config');
+define('MOF_CONTROLLER_DIR', MOF_APP_DIR . '/controller');
 
-require_once(MOF_ROOT_DIR . "/../src/Moflet/Dispacher.php");
+//define('MOF_CORE_DIR', '../../src/');
+define('MOF_CORE_DIR', '{MOF_CORE_DIR}');
+require_once(MOF_CORE_DIR . '/Moflet/Dispacher.php');
 
 $dispacher = new Moflet\Dispacher();
 $dispacher->dispatch();
