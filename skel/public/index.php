080a65fced1761bcd186ddb17842eeae8a2183b1
Taiji Inoue <inudog@gmail.com>
2013-01-26 10:37:53 +0900

    Define some directories.

--- skel/public/index.php
+++ skel/public/index.php
@@ -5,14 +5,20 @@
  */
 
 define('MOF_APP_DIR', dirname(__DIR__));
+
 define('MOF_VIEW_DIR', MOF_APP_DIR . '/view');
 define('MOF_CONFIG_DIR', MOF_APP_DIR . '/config');
 define('MOF_CONTROLLER_DIR', MOF_APP_DIR . '/controller');
+define('MOF_MODEL_DIR', MOF_APP_DIR . '/model');
+define('MOF_TMP_DIR', MOF_APP_DIR . '/var/tmp');
+define('MOF_LOG_DIR', MOF_APP_DIR . '/var/logs');
+define('MOF_CACHE_DIR', MOF_APP_DIR . '/var/cache');
 
-//define('MOF_CORE_DIR', '../../src/');
 define('MOF_CORE_DIR', '{MOF_CORE_DIR}');
-require_once(MOF_CORE_DIR . '/Moflet/Dispacher.php');
+
+$loader = include MOF_APP_DIR . '/vendor/autoload.php';
+
+require_once MOF_CORE_DIR . '/Moflet/Dispacher.php';
 
 $dispacher = new Moflet\Dispacher();
 $dispacher->dispatch();
-
